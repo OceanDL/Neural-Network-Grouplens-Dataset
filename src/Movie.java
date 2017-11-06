4f63a7501af1da77bd3e5bb2e4489cf4d84a9d57
oceandl <oceandouglas.lee@calbaptist.edu>
2017-11-05 17:02:26 -0800

    Dataset and Movie class constructed.

---
+++ src/Movie.java
@@ -0,0 +1,40 @@
+//Movie class to store movie information read in from the JSON file.
+//Ocean Lee 11/5/17
+
+public class Movie {
+    private int budget;
+    private int revenue;
+    private double userRating;
+
+    //Parameterized constructor.
+    public Movie(int budget, int revenue, double userRating){
+        this.budget = budget;
+        this.revenue = revenue;
+        this.userRating = userRating;
+    }
+
+    //Getters and Setters for movie class.
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    public double getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(double userRating) {
+        this.userRating = userRating;
+    }
+}
